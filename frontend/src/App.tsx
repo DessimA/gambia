318c18d8ce7697a0933a0517196099336c6a9b97
DessimA <j.anderson.mect@gmail.com>
2026-07-15 20:24:14 -0300

    feat: scroll-to-top button + remove link 'Inicio' da navbar
    - ScrollToTop: botao circular com ArrowUp, aparece apos 400px de scroll, smooth scroll ao topo
    - Navbar: removido link 'Inicio' (redundante com logo)
    - App.css: estilos do scroll-to-top com animacao fadeInUp e hover

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Cadastrar from './pages/Cadastrar'
@@ -7,13 +9,14 @@ import './App.css'
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastrar" element={<Cadastrar />} />
       </Routes>
-    </>
+    </AuthProvider>
   )
 }
